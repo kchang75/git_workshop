4a069e5beaaf69ce961f40a016ba870348b01d2e
Kristi Chang <kristi.chang@csu.fullerton.edu>
2018-10-10 17:14:47 -0700

    Made prefab, add player script
    platform prefab
    playercontrollercs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(new Vector2(10, 0));
+	}
+}
