ba9bb1c3ee93a36d286b2330d69f79acf9058a2b
Kristi Chang <kristi.chang@csu.fullerton.edu>
2018-10-10 17:38:50 -0700

    public variables, flip sprite,
    quality of life
    public variables
    flip sprite
    building up speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float topSpeed;
+    public float acceleration;
+    public float jumpPower;
+
     Rigidbody2D rb;
+    SpriteRenderer spriteRen;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        spriteRen = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        rb.AddForce(new Vector2(10, 0));
-	}
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(new Vector2(acceleration, 0));
+            spriteRen.flipX = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(new Vector2(-acceleration, 0));
+            spriteRen.flipX = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, jumpPower));
+        }
+
+        if (rb.velocity.x > topSpeed)
+            rb.velocity = new Vector2(topSpeed, rb.velocity.y);
+        else if (rb.velocity.x < -topSpeed)
+            rb.velocity = new Vector2(-topSpeed, rb.velocity.y);
+    }
 }
